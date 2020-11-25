a96fd050f207aa83d1d4f93b352b802c45444766
marsgogo <72649798+marsgogo@users.noreply.github.com>
2020-11-25 18:54:44 +0800

    Update running-mode.js

--- Tools/RunningMode/running-mode.js
+++ Tools/RunningMode/running-mode.js
@@ -1,26 +1,20 @@
 /**
  * Surge & Loon 的运行模式，根据当前网络自动切换模式，此脚本思路来自于Quantumult X。
- * @author: Peng-YM
- * 更新地址: https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/RunningMode/running-mode.js
+ * 更新地址: https://raw.githubusercontent.com/marsgogo/QuanX/master/Tools/RunningMode/running-mode.js
  *
  *************** Surge配置 ***********************
  * 推荐使用模块：
- * https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/RunningMode/running-mode.sgmodule
+ * https://raw.githubusercontent.com/marsgogo/QuanX/master/Tools/RunningMode/running-mode.sgmodule
  * 手动配置：
  * [Script]
- * event network-changed script-path=https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/RunningMode/running-mode.js
+ * event network-changed script-path=https://raw.githubusercontent.com/marsgogo/QuanX/master/Tools/RunningMode/running-mode.js
  *
  *************** Loon配置 ***********************
  * 推荐使用插件：
- * https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/RunningMode/running-mode.plugin
+ * https://raw.githubusercontent.com/marsgogo/QuanX/master/Tools/RunningMode/running-mode.plugin
  * 手动配置：
  * [Script]
- * network-changed script-path=https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tools/RunningMode/running-mode.js
- *
- *************** 脚本配置 ***********************
- * 推荐使用BoxJS配置。
- * BoxJS订阅：https://raw.githubusercontent.com/Peng-YM/QuanX/master/Tasks/box.js.json
- * (不推荐！)手动配置项为config, 请看注释
+ * network-changed script-path=https://raw.githubusercontent.com/marsgogo/QuanX/master/Tools/RunningMode/running-mode.js
  */
 
 let config = {
